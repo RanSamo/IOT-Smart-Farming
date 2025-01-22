2d95b4b5d1f49e98d97290f442edab98183ed9ed
Gil Assenhaym <gil.assenhaym@gmail.com>
2025-01-22 19:07:48 +0200

    Update schema  (#39)
    * add cropTypes
    
    * fix bcrypt
    
    * fix bug
    
    * fix bug
    
    ---------
    
    Co-authored-by: stavsiv <sivilia2@gmail.com>

--- iot-smartfarming-server/routes/userroutes.js
+++ iot-smartfarming-server/routes/userroutes.js
@@ -8,10 +8,10 @@ const router = express.Router();
 router.post("/newUser", async (req, res) => {
   console.log("Creating a new user with data:", req.body);
   try {
-    const { userName, password, fullName, email, phoneNumber, name, location } =
+    const { userName, password, fullName, email, phoneNumber, name, location, cropType } =
       req.body;
 
-    if (!userName || !password || !fullName || !email || !phoneNumber) {
+    if (!userName || !password || !fullName || !email || !phoneNumber || !cropType) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
@@ -23,19 +23,20 @@ router.post("/newUser", async (req, res) => {
         .json({ error: "User with this username or email already exists" });
     }
 
-   // const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newFarm = new Farm({
       name,
       location,
       monitoringData: [],
+      cropType,
     });
     await newFarm.save();
     console.log("New farm created:", newFarm);
 
     const newUser = new User({
       userName,
-      password,// hashedPassword
+      password: hashedPassword,
       fullName,
       email,
       phoneNumber,
@@ -77,35 +78,13 @@ const authenticate = (req, res, next) => {
   }
 };
 
-/*
-// Login endpoint
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "email and password are required." });
-  }
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid email or password." });
-    }
-
-    //const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid password." });
-    }*/
-
       router.post("/login", async (req, res) => {
         const { email, password } = req.body;
       
         if (!email || !password) {
           return res
             .status(400)
-            .json({ message: "email and password are required." });
+            .json({ message: "Email and password are required." });
         }
       
         try {
@@ -114,25 +93,33 @@ router.post("/login", async (req, res) => {
             return res.status(400).json({ message: "Invalid email or password." });
           }
       
-          
-          if (password !== user.password) {
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
             return res.status(400).json({ message: "Invalid password." });
           }
       
-
-    // Create JWT
-    const token = jwt.sign(
-      { userId: user._id, role: user.role }, // Include user ID and optional role
-      process.env.JWT_SECRET,
-      { expiresIn: "3h" }
-    );
-
-    res.json({ token, message: "Login successful." });
-  } catch (err) {
-    console.error("Error during login:", err);
-    res.status(500).json({ message: "Server error." });
-  }
-});
+          const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "3h" }
+          );
+      
+          res.status(200).json({
+            message: "Login successful.",
+            token,
+            user: {
+              email: user.email,
+              fullName: user.fullName,
+              phoneNumber: user.phoneNumber,
+              userName: user.userName,
+            },
+          });
+        } catch (err) {
+          console.error("Error during login:", err);
+          res.status(500).json({ message: "Server error." });
+        }
+      });
+      
 
 // Example: Protected route
 router.get("/protected", authenticate, (req, res) => {

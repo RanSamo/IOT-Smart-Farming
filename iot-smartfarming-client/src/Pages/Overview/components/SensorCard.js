6e5ba44453bd6e0bc249a4bfa2b07f551a17f4f8
TalKlainman <talklainman@gmail.com>
2025-02-15 16:53:01 +0200

    update overview colors

--- iot-smartfarming-client/src/Pages/Overview/components/SensorCard.js
+++ iot-smartfarming-client/src/Pages/Overview/components/SensorCard.js
@@ -20,22 +20,22 @@ import {
 // Function to get the background color and tooltip explanation based on value
 const getBackgroundColor = (value) => {
   if(value == "Good" || value == "On" || value == "Sunny") {
-    return { color: "#90ee90", explanation: "Green: Ideal" };
+    return { color: "#d1fae5", explanation: "Green: Ideal" };
   }
   if(value == "Rainy" || value == "Cloudy" || value == "Fair") {
-    return { color: "#ffcc80", explanation: "Orange: Pay attention" };
+    return { color: "#fff7ed", explanation: "Orange: Pay attention" };
   }
   if(value == "Off" || value == "Stormy") {
-    return { color: "#ff6f61", explanation: "Red: Immediate action needed" };
+    return { color: "#fecaca", explanation: "Red: Immediate action needed" };
   }
 
   const numericValue = parseFloat(value);
   if (numericValue <= 30) {
-    return { color: "#90ee90", explanation: "Green: Ideal" };
+    return { color: "#d1fae5", explanation: "Green: Ideal" };
   } else if (numericValue > 30 && numericValue <= 70) {
-    return { color: "#ffcc80", explanation: "Orange: Pay attention" };
+    return { color: "#fff7ed", explanation: "Orange: Pay attention" };
   } else {
-    return { color: "#ff6f61", explanation: "Red: Immediate action needed" };
+    return { color: "#fecaca", explanation: "Red: Immediate action needed" };
   }
 };
 
